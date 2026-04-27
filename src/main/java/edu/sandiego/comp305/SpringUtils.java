5416585c521b5b8c13de0a09c9054f0f4b6b0ea3
jroberto <jroberto@sandiego.edu>
2026-04-27 00:18:12 -0700

    Feat WIP: class & test method stubs for reverseString

---
+++ src/main/java/edu/sandiego/comp305/SpringUtils.java
@@ -0,0 +1,11 @@
+package edu.sandiego.comp305;
+
+public class SpringUtils {
+
+    public String reverseString(String forwardString){
+
+        String backwardString = "";
+
+        return backwardString;
+    }
+}
---
+++ src/test/java/edu/sandiego/comp305/SpringUtilsTest.java
@@ -0,0 +1,40 @@
+package edu.sandiego.comp305;
+
+import org.junit.jupiter.api.Test;
+
+//import static org.testng.AssertJUnit.assertEquals;
+
+public class SpringUtilsTest {
+
+    @Test
+    public void TestNormalWord() {
+
+    }
+
+    @Test
+    public void TestSentence() {
+
+    }
+
+    @Test
+    public void TestPalindrome() {
+    }
+
+    @Test
+    public void TestSingleChar() {
+
+    }
+
+    @Test
+    public void TestEmpty() {
+
+    }
+
+    @Test
+    public void TestUppercase(){
+
+    }
+
+
+
+}
